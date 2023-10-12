5399aab70cd9c61a70c6a199a147f66f2d2dcf9d
Kun Woo Kim <kimkuns98@gmail.com>
2023-10-12 17:24:50 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {RootStackParamList} from './src/types';
+import SignupScreen from './src/SignupScreen/SignupScreen';
+import AuthProvider from './src/components/AuthProvider';
+import SigninScreen from './src/SigninScreen/SigninScreen';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Screens = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Signin" component={SigninScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Screens />
+    </AuthProvider>
+  );
+};
+
+export default App;
